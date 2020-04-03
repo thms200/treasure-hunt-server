a0e0bf9a365913d27a257efd8db37f0b7b6a101b
thms200 <thms0504@gmail.com>
2020-04-03 15:35:51 +0900

    Add singup and login : save new user in DB, response token etc

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,8 @@
 const errMessage = {
   invalidUrl: 'This is not the web page you are looking for.',
   generalError: 'There was an error on the server. Please try again in a moment.',
+  invalidSignup: 'We can\'t sign up for unknown reasons',
+  invalidLogin: 'We can\'t login for unknown reasons',
 };
 
 module.exports = { errMessage };
--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,3 +1,26 @@
-exports.getLogin = (req, res) => {
-  res.json({ result: 'respond with a users resource' });
+const User = require('../models/Users');
+const jwt = require('jsonwebtoken');
+const { errMessage } = require('../constants');
+
+exports.getLogin = async (req, res) => {
+  try {
+    const { email, picture_url, name } = req.body;
+    const user = await User.findOne({ email });
+    const secretKey = process.env.SECRET_KEY;
+    const options = { expiresIn: '1d', issuer: 'minsun' };
+    const payload = {};
+    if (!user) {
+      const newUser = await User.create({ email, picture_url, name });
+      if (!newUser) return res.json({ result: 'ng', errMessage: errMessage.invalidSignup });
+      payload.name = newUser.name;
+      payload.picture = newUser.picture_url;
+    } else  {
+      payload.name = user.name;
+      payload.picture = user.picture_url;
+    }
+    const token = jwt.sign(payload, secretKey, options);
+    return res.json({ result: 'ok', token });
+  } catch(err) {
+    return res.json({ result: 'ng', errMessage: errMessage.invalidLogin });
+  }
 };
