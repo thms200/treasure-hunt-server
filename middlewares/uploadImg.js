7ab1b00e3a74da2d92d4a5cb4adbd4d475ae92ba
thms200 <thms0504@gmail.com>
2020-04-05 17:52:35 +0900

    Save image in AWS S3

---
+++ middlewares/uploadImg.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2',
+});
+
+const uploadImg = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'my-treasure-hunt',
+    acl: 'public-read-write',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  })
+});
+
+const getImgUrl = (imgsInfo) => imgsInfo.map((img) => img.location);
+
+module.exports = { uploadImg, getImgUrl };
